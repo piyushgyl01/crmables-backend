8611d3441c38f25cf0671f6f516d4217e1254c63
piyush.gyl <piyush2022ug@gmail.com>
2025-01-18 13:29:53 +0530

    feat: templating done for index.js

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+const express = require("express");
+const app = express();
 
-const mongoURI = process.env.MONGODB;
+//DB IMPORT
+const { initialiseDatabase } = require("./db/db.connect.js");
 
-const initialiseDatabase = async () => {
-  await mongoose
-    .connect(mongoURI)
-    .then(() => {
-      console.log("Connected to databse yeahhh");
-    })
-    .catch((error) =>
-      console.log(
-        "You suck lmao here's the error while connecting to db",
-        error
-      )
-    );
+//MODELS IMPORT
+const Lead = require("./models/lead.models.js");
+const Comment = require("./models//comment.models.js");
+const SalesAgent = require("./models/salesAgents.models.js");
+const Tag = require("./models/tag.models.js");
+
+initialiseDatabase();
+
+app.use(express.json());
+
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
 };
 
-module.exports = { initialiseDatabase };
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.send("HELLO, DEVELOPER");
+});
+
+//LEAD CRUD APIs
+
+//SALES AGENT CREATE AND READ APIs
+
+//COMMENTS APIs
+
+//REPORTING APIs
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT yeahhhh ${PORT}`);
+});
