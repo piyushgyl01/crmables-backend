b9a04234f4b3cf6cf1ab4d1f9839e99eb4ebf68b
piyush.gyl <piyush2022ug@gmail.com>
2025-01-19 22:25:20 +0530

    feat: sales agents api done

--- index.js
+++ index.js
@@ -29,7 +29,83 @@ app.get("/", (req, res) => {
 
 //LEAD CRUD APIs
 
+//CREATE API
+app.post("/leads", async (req, res) => {
+  try {
+    const { name, source, salesAgent, status, tags, timeToClose, priority } =
+      req.body;
+
+    const savedLead = new Lead({
+      name,
+      source,
+      salesAgent,
+      status,
+      tags,
+      timeToClose,
+      priority,
+    });
+    await savedLead.save();
+    res.status(200).json(savedLead);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//READ API
+
+//UPDATE API
+
+//DELETE API
+
 //SALES AGENT CREATE AND READ APIs
+
+// async function saveSalesAgentsData(data) {
+//   const savedData = await SalesAgent.insertMany(data);
+//   console.log("Yeah saved these sales agents in the DB", salesAgents)
+//   try {
+//   } catch (error) {
+//     console.log("You suck lmao see this error while saving to DB lol", error);
+//   }
+// }
+
+// saveSalesAgentsData()
+
+//CREATE API
+app.post("/salesAgent", async (req, res) => {
+  try {
+    const { name, email } = req.body;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "Invalid email format" });
+    }
+
+    const existingSalesAgent = await SalesAgent.findOne({ email });
+    if (existingSalesAgent) {
+      res.status(409).json({
+        error: `Sales agent with email ${existingSalesAgent.email} already exists.`,
+      });
+    }
+
+    const savedSalesAgent = new SalesAgent({ name, email });
+    await savedSalesAgent.save();
+    res.status(200).json(savedSalesAgent);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Invalid input: 'email' must be a valid email address." });
+  }
+});
+
+//READ API
+app.get("/salesAgent", async (req, res) => {
+  try {
+    const salesAgents = await SalesAgent.find();
+    res.json(salesAgents);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 //COMMENTS APIs
 
